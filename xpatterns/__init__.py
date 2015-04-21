0b146cdc94d60b8950c2d7609619aa57da0938bc
Charles Hayden <charles.hayden@atigeo.com>
2015-04-21 12:55:55 -0700

    Python path manipulation no longer needed.

--- xpatterns/__init__.py
+++ xpatterns/__init__.py
@@ -1,10 +1,11 @@
 import os
 import sys
 
-if 'SPARK_HOME' in os.environ:
-    spark_home = os.environ['SPARK_HOME']
-    sys.path.insert(0, os.path.join(spark_home, 'python'))
-    sys.path.insert(1, os.path.join(spark_home, 'python/lib/py4j-0.8.2.1-src.zip'))
+# If SPARK_HOME and PYTHONPATH are set appropriately, then these are not needed.
+#if 'SPARK_HOME' in os.environ:
+#    spark_home = os.environ['SPARK_HOME']
+#    sys.path.insert(0, os.path.join(spark_home, 'python'))
+#    sys.path.insert(1, os.path.join(spark_home, 'python/lib/py4j-0.8.2.1-src.zip'))
 
 __all__ = ['xframe', 'xarray', 'xplot', 'sketch']
 
