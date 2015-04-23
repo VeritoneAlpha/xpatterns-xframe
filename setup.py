c6d4a585b83d9625c036e85aa2db58ab14a747c9
Charles Hayden <charles.hayden@atigeo.com>
2015-04-23 13:34:50 -0700

    Add setup control file.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='xframe',
+      version='0.0.1',
+      py_modules=['xpatterns'],
+      author='Charles Hayden',
+      author_email='charles.hayden@atigeo.com',
+      description='XFrame data manipulation for Spark.'
+      )
