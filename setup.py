08a0e38f3b3ea37fbbb2f463b8860d5cd0e462a8
Charles Hayden <charles.hayden@atigeo.com>
2015-05-01 09:07:54 -0700

    Build package.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='xframe',
       version='0.0.1',
-      py_modules=['xpatterns'],
+      packages=['xpatterns'],
       author='Charles Hayden',
       author_email='charles.hayden@atigeo.com',
       description='XFrame data manipulation for Spark.'
