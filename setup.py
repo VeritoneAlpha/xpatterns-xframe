5de005f774c2e825273dc51e09edef1d511599b1
Charles Hayden <charles.hayden@atigeo.com>
2015-05-06 14:46:19 -0700

    Get version from module.

--- setup.py
+++ setup.py
@@ -1,9 +1,38 @@
-from distutils.core import setup
+import os, sys
+import codecs
+
+from setuptools import setup, find_packages
+import xpatterns
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('docs/README.rst')
 
 setup(name='xframe',
-      version='0.0.1',
+      version=xpatterns.__version__,
+      url='https://github.com/Atigeo/xframe',
+      license='Apache Software License 2.0',
       packages=['xpatterns'],
+      include_package_data=True,
+      platforms='any',
       author='Charles Hayden',
       author_email='charles.hayden@atigeo.com',
-      description='XFrame data manipulation for Spark.'
+      description='XFrame data manipulation for Spark.',
+      classifiers=['Programming Language :: Python',
+                   'Development Status :: 4 - Beta',
+                   'Natural Language :: English',
+                   'Environment :: ???',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Apache Software License 2.0',
+                   'Operating System :: OS Independent',
+                   'Topic :: Software Development :: Libraries :: Python Modules']
       )
