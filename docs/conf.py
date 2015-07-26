0736734f6832c8e5657fe24f6ff14d60aa5fb716
Charles Hayden <charles.hayden@atigeo.com>
2015-07-26 12:55:06 -0700

    Try matplotlib.pyplot mock.

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['numpy', 'pandas', 'matplotlib', 'pyspark', 'pyspark.sql', 'pyspark.sql.types']
+MOCK_MODULES = ['numpy', 'pandas', 'matplotlib', 'pyspark', 'pyspark.sql', 'pyspark.sql.types', 'matplotlib.pyplot']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
